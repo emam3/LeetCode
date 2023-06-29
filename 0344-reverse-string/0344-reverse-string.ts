23bfa9c8fdf761e05b07db8712facb9d014e0a47
NourEldin Ahmed <67537653+emam3@users.noreply.github.com>
2023-06-29 19:09:15 +0300

    Time: 101 ms (33.11%), Space: 49.2 MB (96.99%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.ts
+++ 0344-reverse-string/0344-reverse-string.ts
@@ -2,11 +2,13 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    let newArr: string[] = [...s];
-    let j: number = s.length -1;
-
-    for(let i:number = 0; i < s.length ; i++) {
-        s[i] = newArr[j];
-        j--;
+    let left = 0
+    let right = s.length - 1
+    while(left <= right) {
+        let temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+        left++
+        right--
     }
 };
