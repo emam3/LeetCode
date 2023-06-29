e6e48ef39bffd1830cbeef3f70a1b6e39d2177ab
NourEldin Ahmed <67537653+emam3@users.noreply.github.com>
2023-06-29 20:28:00 +0300

    Time: 90 ms (8.25%), Space: 45.7 MB (72.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,12 +1,27 @@
 function majorityElement(nums: number[]): number {
     let elements: any = {};
-    nums.map((num: number) => {
+    let n: number = Math.floor(nums.length);
+    
+    for(let i:number = 0; i< nums.length ; i++) {
+        let num:number = nums[i];
+        
         if(elements[String(num)] !== undefined) {
             elements[String(num)]++;
+            if(elements[String(num)] > n) return elements[String(num)]
         } else {
             elements[String(num)] = 0;
         }
-    });
+    }
+    
+//     nums.map((num: number) => {
+    
+//         if(elements[String(num)] !== undefined) {
+//             elements[String(num)]++;
+//         } else {
+//             elements[String(num)] = 0;
+//         }
+        
+//     });
     
     return Number(Object.keys(elements).reduce((a, b) => elements[a] > elements[b] ? a : b));
     
