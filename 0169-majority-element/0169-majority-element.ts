7e15622d26eb22bcb4ab1795299dfa89bf24f708
NourEldin Ahmed <67537653+emam3@users.noreply.github.com>
2023-06-29 20:52:30 +0300

    Time: 2169 ms (5.02%), Space: 45.8 MB (72.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,28 +1,35 @@
+const insertionSort = (arr) => {
+ for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[i]) {
+                let temp = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp
+            }
+        }
+    }
+    return arr;
+}
+
 function majorityElement(nums: number[]): number {
     let elements: any = {};
     let n: number = Math.floor(nums.length);
+    let sortedArr: number[] = insertionSort(nums);
+    return sortedArr[Math.floor(n/2)];
     
-    for(let i:number = 0; i< nums.length ; i++) {
-        let num:number = nums[i];
+    // for(let i:number = 0; i< nums.length ; i++) {
+//         let num:number = nums[i];
         
-        if(elements[String(num)] !== undefined) {
-            elements[String(num)]++;
-            if(elements[String(num)] > n) return elements[String(num)]
-        } else {
-            elements[String(num)] = 0;
-        }
-    }
-    
-//     nums.map((num: number) => {
-    
 //         if(elements[String(num)] !== undefined) {
 //             elements[String(num)]++;
+//             if(elements[String(num)] > n) return elements[String(num)]
 //         } else {
 //             elements[String(num)] = 0;
 //         }
-        
-//     });
     
-    return Number(Object.keys(elements).reduce((a, b) => elements[a] > elements[b] ? a : b));
+}
     
-};
+
+    
+    // return Number(Object.keys(elements).reduce((a, b) => elements[a] > elements[b] ? a : b));
+    
