4643a48d17a15ff8e9bac7444977aa194b8f26eb
NourEldin Ahmed <67537653+emam3@users.noreply.github.com>
2024-02-24 03:02:53 +0200

    Time: 62 ms (51.66%), Space: 53.8 MB (30.69%) - LeetHub

--- 0704-binary-search/0704-binary-search.ts
+++ 0704-binary-search/0704-binary-search.ts
@@ -1,10 +1,9 @@
 function search(nums: number[], target: number): number {
     let start: number = 0;
     let end: number = nums.length -1;
-    let mid: number;
     
     do {
-        mid = Math.floor(start + (end - start) / 2);
+        let mid = Math.floor(start + (end - start) / 2);
         if(nums[mid] === target) return mid;
         else if(nums[mid] < target) {
             start = mid +1;
